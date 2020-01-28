c845e01aa1b7bf6a99b228df350c0652fd07c56f
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2020-01-28 08:28:24 -0700

    Agregar archivos de proyecto.

---
+++ PlantillaGraficos.cpp
@@ -0,0 +1,133 @@
+// PlantillaGraficos.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define GLEW_STATIC
+
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+#include <iostream>
+
+using namespace std;
+
+float angulo = 0.0f;
+
+void actualizar() {
+	angulo += 0.01;
+	if (angulo >= 360.0f) {
+		angulo = 0.0f;
+	}
+}
+
+void dibujarTriangulo() {
+	glPushMatrix();
+
+	//glTranslatef(0.0f, -0.30f, 0.0f);
+	//glRotatef(angulo, 0.0f, 1.0f, 0.0f);
+	//glScalef(0.5f, 2.0f, 1.0f);
+
+	glBegin(GL_TRIANGLES);
+
+	glColor3f(0.7f, 0.1f, 0.8f);
+	glVertex3f(0.0f, 0.15f, 0.0f);
+	glVertex3f(-0.15f, -0.15f, 0.0f);
+	glVertex3f(0.15f, -0.15f, 0.0f);
+
+	glEnd();
+	glPopMatrix();
+	glPushMatrix();
+
+	//glTranslatef(0.0f, -0.30f, 0.0f);
+	//glRotatef(angulo, 0.0f, 1.0f, 0.0f);
+	//glScalef(0.5f, 2.0f, 1.0f);
+
+	glBegin(GL_TRIANGLES);
+
+	glColor3f(0.7f, 0.1f, 0.8f);
+	glVertex3f(0.0f, 0.15f, 0.0f);
+	glVertex3f(-0.15f, -0.15f, 0.0f);
+	glVertex3f(0.15f, -0.15f, 0.0f);
+
+	glEnd();
+	glPopMatrix();
+
+
+}
+
+void dibujar() {
+	dibujarTriangulo();
+	
+ 
+}
+
+int main()
+{
+//Declarar una ventana
+	GLFWwindow* window;
+
+	//si no se pudo iniciar GLFW
+	//Terminamos ejecucion
+	if (!glfwInit()) {
+		exit(EXIT_FAILURE);
+	}
+	// si se pudo iniciar GLFW
+	//Inicalizamos la ventana
+
+	window = glfwCreateWindow(600, 600, "Ventana", NULL, NULL);
+
+	// si no se pudo crear la ventana
+	// terminamos ejecucuon
+	if (!window) {
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+	//Establecemos la ventana con contexto
+	glfwMakeContextCurrent(window);
+
+	//Una vez establecido el contexto se activan las funciones "modernas" (gpu)
+	glewExperimental = true;
+
+	GLenum errores = glewInit();
+	if (errores != GLEW_OK) {
+		glewGetErrorString(errores);
+	}
+
+	const GLubyte* versionGL = glGetString(GL_VERSION);
+	cout << "Version openGL:" << versionGL;
+
+
+	//Ciclo de dibujo (Draw loop)
+	while (!glfwWindowShouldClose(window)) {
+		//Establecer region de dibujo
+		glViewport(0, 0, 600, 600);
+		//Establecer el color de borrado
+		glClearColor(1, 0.8, 0, 1);
+		//Borrar!
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		//actualizar valores y dibujar
+		actualizar();
+		dibujar();
+
+
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+	//Despúes del ciclo de dibujo
+	//Eliminamos venta y procesos de glfw
+	glfwDestroyWindow(window);
+	glfwTerminate;
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
